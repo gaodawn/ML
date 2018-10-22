3c37e8395b7dc812a7700ae4b977f5fa9ba55697
gaodawn <gaodawn@qq.com>
2018-10-22 16:10:52 +0800

    modify

--- Naive Bayes/bayesgao.py
+++ Naive Bayes/bayesgao.py
@@ -14,13 +14,13 @@ def loadDataSet():
                  ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
     classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
     return postingList,classVec
-                 
+ #创建词汇表                
 def createVocabList(dataSet):
     vocabSet = set([])  #create empty set
     for document in dataSet:
         vocabSet = vocabSet | set(document) #union of the two sets
     return list(vocabSet)
-
+#词汇转成向量
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
@@ -28,31 +28,46 @@ def setOfWords2Vec(vocabList, inputSet):
             returnVec[vocabList.index(word)] = 1
         else: print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
-
+#训练数据
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = ones(numWords); p1Num = ones(numWords)      #change to ones() 
+    p0Num = ones(numWords); p1Num = ones(numWords)  
+    #p0Num = zeros(numWords); p1Num = zeros(numWords)  
+        #change to ones() 
+    #p0Denom = 0.0; p1Denom = 0.0   
     p0Denom = 2.0; p1Denom = 2.0                        #change to 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
+            print("i:")
+            print(i)
+            print("p1Num:")
+            print(p1Num)
             p1Denom += sum(trainMatrix[i])
         else:
+            
             p0Num += trainMatrix[i]
+            print("i:")
+            print(i)
+            print("p0Num:")
+            
             p0Denom += sum(trainMatrix[i])
+            print(p0Num)
+    #p1Vect = p1Num/p1Denom         #change to log()
+    #p0Vect = p0Num/p0Denom          #change to log()
     p1Vect = log(p1Num/p1Denom)          #change to log()
     p0Vect = log(p0Num/p0Denom)          #change to log()
     return p0Vect,p1Vect,pAbusive
-
+#分类
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) + log(pClass1)    #element-wise mult
     p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
     if p1 > p0:
-        return 1
+        return 1,p1,p0
     else: 
-        return 0
+        return 0,p0,p1
     
 def bagOfWords2VecMN(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
@@ -171,24 +186,36 @@ def getTopWords(ny,sf):
         print(item[0])
 if __name__ == '__main__':
     listOPosts,listClasses=loadDataSet()
-    print("留言列表：")
+    print("留言列表：ListOposts")
     print(listOPosts)
-    print("分类列表：")
+    print("分类列表：listClasses")
     print(listClasses)
     myVocabList=createVocabList(listOPosts)
-    print("我的词汇表：")
+    print("我的词汇表：myVocabList")
     print(myVocabList)
-    print("文档向量1：")
+    print("文档向量1：setOfWords2Vec[0]")
     print(setOfWords2Vec(myVocabList, listOPosts[0]))
-    print("文档向量2：")
+    print("文档向量2：setOfWords2Vec[1]")
     print(setOfWords2Vec(myVocabList, listOPosts[1]))
     trainMat=[]
     for postinDoc in listOPosts:
         trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
-    print("训练向量：")
-    print(trainMat)
+    print("训练向量：trainMat" )
+    print(trainMat) 
+    print("训练向量：len(trainMat)" )
+    print(len(trainMat))
+    words1=len(trainMat[0])
+    print("words1:")
+    print(words1)
+    numtraindocs=len(trainMat)
+    print("len(trainMat):")
+    print(numtraindocs)
+    print("sum(listClasses):")
+    print(sum(listClasses))
     p0V,p1V,pAb=trainNB0(trainMat,listClasses)
+    #train result
     print(p0V)
     print(p1V)
     print(pAb)
+    #test
     testingNB()
